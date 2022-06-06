5702d7c25112c51836b6e9de69f69ef5bc564696
Marina Kobozeva <kobozevamf@gmail.com>
2022-06-07 00:34:07 +0300

    add dotenv

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,6 @@
-const SECRET_KEY = 'E4t%y&ui*k';
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const SECRET_KEY = NODE_ENV === 'production' ? JWT_SECRET : 'E4t%y&ui*k';
 const DB_LINK = 'mongodb://localhost:27017/moviesdb';
 
 module.exports = {
