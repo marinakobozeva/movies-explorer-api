91360e2bbe725637d327f01005e6c2b15aedb864
Marina Kobozeva <kobozevamf@gmail.com>
2022-08-03 01:35:44 +0300

    Add CORS and increase rate limit

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 100000,
   standardHeaders: true,
   legacyHeaders: false,
 });
